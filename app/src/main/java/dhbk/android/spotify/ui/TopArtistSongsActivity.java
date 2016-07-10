e4d61d2a47dec63f9b574667afe733dd19c009af
Phong <p7157568@gmail.com>
2016-07-10 23:55:12 +0700

    feat - implement method in top song activiy

--- app/src/main/java/dhbk/android/spotify/ui/TopArtistSongsActivity.java
+++ app/src/main/java/dhbk/android/spotify/ui/TopArtistSongsActivity.java
@@ -1,14 +1,31 @@
 package dhbk.android.spotify.ui;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 import dhbk.android.spotify.R;
+import dhbk.android.spotify.fragments.TopArtistSongsFragment;
 
 public class TopArtistSongsActivity extends BaseActivity {
 
+    // TODO: 7/10/16 this is a oncreate activity
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_top_artist_songs);
+    int contentViewId() {
+        return R.layout.activity_top_artist_songs;
+    }
+
+    @Override
+    boolean activateBackButton() {
+        return true;
+    }
+
+    @Override
+    protected void onCreateActivity(@Nullable Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            if (getIntent().getExtras() != null) {
+                TopArtistSongsFragment topArtistSongsFragment = (TopArtistSongsFragment) getSupportFragmentManager().findFragmentById(R.id.top_artist_songs_fragment);
+                topArtistSongsFragment.onSearchArtistTopTracks(getIntent().getExtras().getString("artist_id"));
+            }
+        }
     }
 }
