6d4455f8d8f07344c62e67da8b46c171c1ec6c6c
Phong <p7157568@gmail.com>
2016-07-10 21:56:42 +0700

    feat - change viewholder in adapter

--- app/src/main/java/dhbk/android/spotify/ArtistSearchAdapter.java
+++ app/src/main/java/dhbk/android/spotify/ArtistSearchAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -21,14 +22,14 @@ public class ArtistSearchAdapter extends CommonAdapter<Artist> {
 
         ArtistsSearchHolder artistsSearchHolder;
         if (view == null) {
-            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.spotify_streamer_row_layout,viewGroup,false);
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.spotify_streamer_row_layout, viewGroup, false);
             artistsSearchHolder = new ArtistsSearchHolder(view);
             view.setTag(artistsSearchHolder);
-        }else{
-            artistsSearchHolder = (ArtistsSearchHolder)view.getTag();
+        } else {
+            artistsSearchHolder = (ArtistsSearchHolder) view.getTag();
         }
 
-        if(getItems().get(i).images.size() >0 && getItems().get(i).images.get(1).url != null) {
+        if (getItems().get(i).images.size() > 0 && getItems().get(i).images.get(1).url != null) {
             Picasso.with(viewGroup.getContext())
                     .load(getItems().get(i).images.get(1).url)
                     .resize(200, 200)
@@ -39,14 +40,15 @@ public class ArtistSearchAdapter extends CommonAdapter<Artist> {
         return view;
     }
 
+
     public static class ArtistsSearchHolder {
-        @InjectView(R.id.artist_image)
+        @BindView(R.id.artist_image)
         ImageView artistsImage;
-        @InjectView(R.id.artist_name)
+        @BindView(R.id.artist_name)
         TextView artistsName;
 
-        public ArtistsSearchHolder(View view){
-            ButterKnife.inject(this,view);
+        public ArtistsSearchHolder(View view) {
+            ButterKnife.bind(this, view);
         }
     }
 }
