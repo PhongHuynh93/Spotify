19c0a79516cfc83dc741d634f5fa787206d77e04
Phong <p7157568@gmail.com>
2016-07-10 23:57:20 +0700

    feat - change code in MainActivity

--- app/src/main/java/dhbk/android/spotify/ui/MainActivity.java
+++ app/src/main/java/dhbk/android/spotify/ui/MainActivity.java
@@ -1,18 +1,20 @@
 package dhbk.android.spotify.ui;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 
 import dhbk.android.spotify.R;
 
 public class MainActivity extends BaseActivity {
+    // TODO: 7/10/16 this is oncreate view
+    @Override
+    int contentViewId() {
+        return R.layout.activity_main;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    boolean activateBackButton() {
+        return false;
     }
 
     // TODO: 7/10/16 start spotify
