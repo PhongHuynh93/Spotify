d061f14ecccf51220bc93cb5c0acaf5c71d83f88
Phong <p7157568@gmail.com>
2016-07-10 20:25:50 +0700

    feat - add search activity and layout

--- app/src/main/java/dhbk/android/spotify/MainActivity.java
+++ app/src/main/java/dhbk/android/spotify/MainActivity.java
@@ -1,7 +1,9 @@
 package dhbk.android.spotify;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    // TODO: 7/10/16 start spotify
+    public void onStartSpotifyStreamer(View view) {
+        Intent spotifySearch = new Intent(MainActivity.this, SearcherActivity.class);
+        startActivity(spotifySearch);
     }
 
 }
