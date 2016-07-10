b799f84197e5a3ff89cd1f86d96e1a68739711e5
Phong <p7157568@gmail.com>
2016-07-10 20:36:19 +0700

    feat - implement onsearchItemClick

--- app/src/main/java/dhbk/android/spotify/SearcherActivity.java
+++ app/src/main/java/dhbk/android/spotify/SearcherActivity.java
@@ -1,12 +1,27 @@
 package dhbk.android.spotify;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SearcherActivity extends BaseActivity {
+public class SearcherActivity extends BaseActivity implements OnSearchItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searcher);
+    }
+
+    @Override
+    public void onSearchItemClick(String artistId) {
+        TopArtistSongsFragment topArtistSongsFragment = (TopArtistSongsFragment)getSupportFragmentManager().findFragmentById(R.id.top_artist_songs_fragment);
+        if(topArtistSongsFragment == null){
+            Intent topArtistSongsIntent = new Intent(SearcherActivity.this, TopArtistSongsActivity.class);
+            topArtistSongsIntent.putExtra("artist_id", artistId);
+            startActivity(topArtistSongsIntent);
+            saveTypeDevice(false);
+        }else{
+            saveTypeDevice(true);
+            topArtistSongsFragment.onSearchArtistTopTracks(artistId);
+        }
     }
 }
