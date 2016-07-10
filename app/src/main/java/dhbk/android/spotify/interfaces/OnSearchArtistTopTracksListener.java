e7741b7a3e361a784c515fc09e3b4b2c94cbc22e
Phong <p7157568@gmail.com>
2016-07-10 23:37:30 +0700

    feat - add interface

---
+++ app/src/main/java/dhbk/android/spotify/interfaces/OnSearchArtistTopTracksListener.java
@@ -0,0 +1,8 @@
+package dhbk.android.spotify.interfaces;
+
+/**
+ * Created by huynhducthanhphong on 7/10/16.
+ */
+public interface OnSearchArtistTopTracksListener {
+    void onSearchArtistTopTracks(String artistId);
+}
